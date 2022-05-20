de5fde2f9b0d114171eae23ec825015eaf26276f
gpmike31 <gpmike3105@gmail.com>
2022-05-20 14:37:19 -0500

    finished reformatting jumbotron information

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -51,64 +51,6 @@ const SavedBooks = () => {
     return <h2>LOADING...</h2>;
   }
 
-
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
-
   return (
     <>
       <Jumbotron fluid className='text-light bg-dark'>
@@ -130,9 +72,10 @@ const SavedBooks = () => {
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
+                  {book.link ? <Card.Text><a href={book.link} target="_blank" rel="noopener noreferrer" >More Information on Google Books</a></Card.Text> : null}
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book!
+                  <Button className='btn-block btn-danger' onClick={handleDeleteBook}>
+                    Delete this Book
                   </Button>
                 </Card.Body>
               </Card>
